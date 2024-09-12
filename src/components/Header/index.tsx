db4f612258061ea017c4151e5c5ae4d8956cc674
d m <dmontone@gmail.com>
2024-09-12 14:33:27 -0300

    Moves all styles to specific files for segregation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,8 @@
-import styled from "styled-components";
+import { ReactNode } from 'react'
+import * as S from './styles'
 
-export const Header = styled.header`
-  background: rgb(255, 117, 0);
-  background: linear-gradient(
-    258deg,
-    rgba(255, 117, 0, 1) 8%,
-    rgba(232, 5, 55, 1) 53%
-  );
-  width: 100%;
-  height: 64px;
-  position: fixed;
-  top: 0;
-  display: flex;
-  align-items: center;
-  padding:0px 24px;
+type HeaderType = {
+  children: ReactNode
+}
 
-  h1 {
-    color: #fff;
-    font-size: 24px;
-  }
-`;
+export const Header = ({ children }: HeaderType) => <S.Header>{children}</S.Header>
