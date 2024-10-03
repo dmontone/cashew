02f7696c204bf03706edc241a75f65da5589c32c
d m <dmontone@gmail.com>
2024-10-03 15:14:32 -0300

    Adds refresh feature

--- src/pages/Dashboard/components/Searchbar/search-bar.test.tsx
+++ src/pages/Dashboard/components/Searchbar/search-bar.test.tsx
@@ -2,6 +2,9 @@ import { render, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { SearchBar } from './search-bar'
 import routes from '~/router/routes'
+import { handleApi } from '~/utils'
+import { DashContext } from '~/context'
+import { act } from 'react'
 
 const mockHistoryPush = jest.fn()
 jest.mock('react-router-dom', () => ({
@@ -11,7 +14,22 @@ jest.mock('react-router-dom', () => ({
   }),
 }))
 
+jest.mock('~/utils', () => ({
+  handleApi: {
+    getAll: jest.fn()
+  }
+}))
+const mockGetAll = handleApi.getAll as jest.Mock
+
 describe('components/search-bar', () => {
+  const user = userEvent.setup()
+  const mockDispatch = jest.fn()
+  const renderWithContext = (value: any = {}) => render(
+    <DashContext.Provider value={[value, mockDispatch]}>
+      <SearchBar />
+    </DashContext.Provider>
+  )
+
   beforeEach(() => {
     jest.clearAllMocks()
   })
@@ -20,15 +38,45 @@ describe('components/search-bar', () => {
     render(<SearchBar />)
 
     const input = screen.getByPlaceholderText(/Digite um CPF válido/)
+    const refreshButton = screen.getByLabelText(/refetch/)
     const newButton = screen.getByText(/Nova Admissão/)
 
     expect(input).toBeInTheDocument()
+    expect(refreshButton).toBeInTheDocument()
     expect(newButton).toBeInTheDocument()
   })
-  
+
+  it('should api call on refresh button click', async () => {
+    mockGetAll.mockResolvedValueOnce({ data: 'TEST_DATA', error: null })
+    renderWithContext()
+
+    const newButton = screen.getByLabelText(/refetch/)
+    await user.click(newButton)
+
+    expect(mockDispatch).toHaveBeenCalledWith({ type: 'FETCH_CARDS_INIT' })
+    await waitFor(() => {
+      expect(mockDispatch).toHaveBeenCalledWith({ type: 'FETCH_CARDS_SUCCESS', payload: 'TEST_DATA' })
+      expect(mockGetAll).toHaveBeenCalled()
+    })
+  })
+
+  it('should api call error on refresh button click', async () => {
+    mockGetAll.mockResolvedValueOnce({ data: null, error: 'TEST_ERROR' })
+    renderWithContext()
+
+    const newButton = screen.getByLabelText(/refetch/)
+    await user.click(newButton)
+
+    expect(mockDispatch).toHaveBeenCalledWith({ type: 'FETCH_CARDS_INIT' })
+    await waitFor(() => {
+      expect(mockGetAll).toHaveBeenCalled()
+      expect(mockDispatch).toHaveBeenCalledWith({ type: 'FETCH_CARDS_ERROR', payload: 'TEST_ERROR' })
+    })
+  })
+
   it('should push new url on button click', async () => {
     render(<SearchBar />)
-    
+
     const newButton = screen.getByText(/Nova Admissão/)
     userEvent.click(newButton)
 
--- src/pages/Dashboard/components/Searchbar/search-bar.tsx
+++ src/pages/Dashboard/components/Searchbar/search-bar.tsx
@@ -3,19 +3,30 @@ import { useHistory } from "react-router-dom"
 import { Button, IconButton, TextField } from "~/components"
 import routes from "~/router/routes"
 import * as S from "./styles"
+import { DashContext } from '~/context'
+import { useContext } from 'react'
+import { handleApi } from '~/utils'
 
 export const SearchBar = () => {
+  const [, dispatch] = useContext(DashContext)
   const history = useHistory()
 
   const goToNewAdmissionPage = () => {
     history.push(routes.newUser)
   }
 
+  const reloadData = async () => {
+    dispatch({ type: 'FETCH_CARDS_INIT' })
+    const { data, error } = await handleApi.getAll()
+    if (data) dispatch({ type: 'FETCH_CARDS_SUCCESS', payload: data })
+    if (error) dispatch({ type: 'FETCH_CARDS_ERROR', payload: error })
+  }
+
   return (
     <S.Container>
       <TextField placeholder="Digite um CPF válido" />
       <S.Actions>
-        <IconButton aria-label="refetch">
+        <IconButton aria-label="refetch" onClick={reloadData}>
           <HiRefresh />
         </IconButton>
         <Button onClick={() => goToNewAdmissionPage()}>Nova Admissão</Button>
